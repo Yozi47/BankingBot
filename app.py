24907d7297223fc86190d252792b44bb92062537
Priya <priya.chelluboyina@mnsu.edu>
2023-04-27 00:13:34 -0500

    rename html page for landingpage

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def landingpage():
-    return render_template("new_home.html")
+    return render_template("landingpage.html")
 
 @app.route("/login.html")
 def login():
